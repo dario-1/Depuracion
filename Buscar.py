ec2c8c9abd9ae3d05aec47a16668a83720925694
dario-1 <renancitosalazar@gmail.com>
2023-09-16 23:41:14 -0500

    Filtrado2

--- Buscar.py
+++ Buscar.py
@@ -8,10 +8,11 @@ def buscar():
     label1.config(text=archivo)
     if archivo:
         df = pd.read_csv(archivo)
-        print(df)
-    columnas=df.columns
-    print(columnas)
-    print(df[['Abs (Corr)1','Abs (Corr)2','Abs (Corr)3']])
+        #print(df)
+        columnas=df.columns
+        #print(columnas)
+        tabla=df[['Abs (Corr)1','Abs (Corr)2','Abs (Corr)3']]
+        print(tabla)
     
 root = tk.Tk()
 root.title('Buscador')
@@ -19,9 +20,7 @@ archivo = ""  # Variable global para almacenar la ruta del archivo
 
 button1 = tk.Button(root, text='Buscar', command=buscar)
 button1.pack()
-
 label1 = tk.Label(root, text="")
 label1.pack()
-
 
 root.mainloop()
