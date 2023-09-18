3de13c5bd2f0d4c1113b4650ebdebdfb8cc77d85
dario-1 <renancitosalazar@gmail.com>
2023-09-18 10:52:15 -0500

    interfaz2

--- Buscar.py
+++ Buscar.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
 import tkinter as tk
 import pandas as pd
+from tkinter import font
 from tkinter import ttk
+from tkinter import Menu
 from tkinter import filedialog
 import os
+
+def font1(size):
+     return font.Font(family="Times New Roman", size=size)
+    
+
+def seleccionar_opcion():
+    seleccion = variable.get()
+    if seleccion == "Maquina 1":
+        etiqueta.config(text="Has seleccionado la Maquina 1")
+    elif seleccion == "Maquina 2":
+        etiqueta.config(text="Has seleccionado la Maquina 2")
+    
 
 def buscar():
     try:
@@ -40,12 +55,27 @@ def buscar():
         label1.config(text=f"Error: {str(e)}")
 
 root = tk.Tk()
-root.title('Depurador de archivos')
 
-button1 = tk.Button(root, text='Buscar', command=buscar)
-button1.pack()
+root.title('Depurador de archivos')
+root.geometry("1587x850")
+#fuente=font.Font(family="Times New Roman",size=18)
+encabezado1 = tk.Label(root, text="SELECCION DE MAQUINA:",font=font1(10))
+encabezado1.grid(row=0, column=0, padx=20, pady=20, columnspan=2)
+opciones = ["Maquina 1", "Maquina 2"]
+variable = tk.StringVar(root)
+variable.set("Seleccione el equipo")
+menu_desplegable = tk.OptionMenu(root, variable, *opciones)
+menu_desplegable.config(width=25, height=1)  # Ancho de 120 y alto de 8
+#menu_desplegable.pack(side="left")  # Colocar a la izquierda
+menu_desplegable.grid(row=1, column=0, padx=20, pady=5, sticky="w")
+
+button1 = tk.Button(root, text='BUSCAR',font=font.Font(family="Times New Roman",size=12), command=buscar)
+#button1.pack()
+button1.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+
+
 
 label1 = tk.Label(root, text="")
-label1.pack()
+label1.grid(row=3, column=0, padx=20, pady=5, sticky="w")
 
 root.mainloop()
